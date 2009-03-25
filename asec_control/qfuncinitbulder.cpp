ce2f8ad8403d6f852b8254f1cb62b5c1e2e00cfb
Nikolay Yakimov <root@livid.pp.ru>
2009-03-25 20:54:18 +0000

    changed identation

--- asec_control/qfuncinitbulder.cpp
+++ asec_control/qfuncinitbulder.cpp
@@ -9,39 +9,39 @@
 
 QFuncInitBuilder::QFuncInitBuilder(QString service) : QMethodFetcher(service, "ru.pp.livid.asec.exports")
 {
-	code="%4.%1 = function(%3) { call(\"%1\", \"%2\"%5%3); }; ";
-	object=service;
-	object.replace("ru.pp.livid.asec.","");
-	parse();
-};
+    code="%4.%1 = function(%3) { call(\"%1\", \"%2\"%5%3); }; ";
+    object=service;
+    object.replace("ru.pp.livid.asec.","");
+    parse();
+}
 
 QFuncInitBuilder::~QFuncInitBuilder()
 {
 
-};
+}
 
 
 void QFuncInitBuilder::new_method(QString name, int argcount)
 {
-	method_name=name;
-	method_args="";
-	method_args_count=argcount;
-	method_type="";
-	if(argcount==0)
-		init+=code.arg(method_name,service,"",object,"");
+    method_name=name;
+    method_args="";
+    method_args_count=argcount;
+    method_type="";
+    if(argcount==0)
+        init+=code.arg(method_name,service,"",object,"");
 }
 
 void QFuncInitBuilder::new_arg(QString name, QString type, QString direction)
 {
-	method_args_count--;
-	if(direction=="out" && name=="")
-		method_type=type;
-	else
-	{
-		method_args+=name;
-		if(method_args_count>0)
-			method_args+=",";
-		else
-			init+=code.arg(method_name,service,method_args,object,",");
-	}
+    method_args_count--;
+    if(direction=="out" && name=="")
+        method_type=type;
+    else
+    {
+        method_args+=name;
+        if(method_args_count>0)
+            method_args+=",";
+        else
+            init+=code.arg(method_name,service,method_args,object,",");
+    }
 }
