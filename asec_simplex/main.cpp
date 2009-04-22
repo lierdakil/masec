0a814c53965ffae9f0da127dd9a49ccc68b07a2e
Nikolay Yakimov <root@livid.pp.ru>
2009-04-22 23:44:55 +0000

    Fixed 'toy mode' in asec_simplex

--- asec_simplex/main.cpp
+++ asec_simplex/main.cpp
@@ -6,74 +6,6 @@
 #include <iostream>
 #include "graph.h"
 #include <QApplication>
-#include <point2d.h>
-#include <gsl/gsl_math.h>
-#include <gsl/gsl_multimin.h>
-
-#define PI 3.1415926535
-#define R2 33000
-#define PARAM_COUNT 6
-
-struct param_struct {
-    QVector<Point2D> *data;
-    double f;
-    double f_min;
-    double f_max;
-    double RmU,LmU,CmU,C0U,UU,R0U;
-    double Rm,Lm,Cm,C0,U,R0;
-};
-
-double If(const gsl_vector *v, void *params) //I(f)
-{
-    param_struct *p = (param_struct*)params;
-
-    double Rm,Lm,Cm,C0,U,R0;
-    if(p->Rm<0) Rm=fabs(gsl_vector_get(v,0))*p->RmU;
-    else Rm=p->Rm;
-    if(p->Lm<0) Lm=fabs(gsl_vector_get(v,1))*p->LmU;
-    else Lm=p->Lm;
-    if(p->Cm<0) Cm=fabs(gsl_vector_get(v,2))*p->CmU;
-    else Cm=p->Cm;
-    if(p->C0<0) C0=fabs(gsl_vector_get(v,3))*p->C0U;
-    else C0=p->C0;
-    if(p->U<0) U=fabs(gsl_vector_get(v,4))*p->UU*R2;
-    else U=p->U*R2;//we actually need not U, but U*R2
-    if(p->R0<0) R0=fabs(gsl_vector_get(v,5))*p->R0U;
-    else R0=p->R0;
-
-    if(Rm==0 || Lm==0 || Cm==0 || C0==0)
-        return GSL_NAN;
-
-    double f0 = 1/(2*PI*sqrt(Lm*Cm));
-
-    if(f0<(p->f_min) || f0>(p->f_max))
-        return U/R0;//it's actually the theoretical maximum
-
-    double w=2*PI*p->f;
-
-    double A = (((pow(Cm,2)*pow(w,4)*pow(Rm,2)+pow(Cm,2)*pow(w,6)*pow(Lm,2)-2*Cm*pow(w,4)*Lm+pow(w,2))*pow(C0,2)+(2*Cm*pow(w,2)-2*pow(Cm,2)*pow(w,4)*Lm)*C0+pow(Cm,2)*pow(w,2))*pow(R0,2)+2*pow(Cm,2)*pow(w,2)*Rm*R0+pow(Cm,2)*pow(w,2)*pow(Rm,2)+pow(Cm,2)*pow(w,4)*pow(Lm,2)-2*Cm*pow(w,2)*Lm+1)/((pow(Cm,2)*pow(w,4)*pow(Rm,2)+pow(Cm,2)*pow(w,6)*pow(Lm,2)-2*Cm*pow(w,4)*Lm+pow(w,2))*pow(C0,2)+(2*Cm*pow(w,2)-2*pow(Cm,2)*pow(w,4)*Lm)*C0+pow(Cm,2)*pow(w,2));
-    if(A>0)
-        return U/sqrt(A);
-    else
-        return GSL_NAN;
-}
-
-double StDev(const gsl_vector *v, void *params) //sum from 0 to N-1 (I_exp(f)-I(f))**2
-{
-    param_struct *p = (param_struct*)params;
-    QVector<Point2D> *data = p->data;
-    double S=0;
-    for(int i=0; i<data->count(); ++i)
-    {
-        p->f = data->at(i).x;
-        double I = If(v,params);
-        if(I==GSL_NAN)
-            return I;
-        double r = (data->at(i).y-I);
-        S+=r*r;
-    }
-    return S;
-}
 
 Point2D find_extremum(QVector<Point2D> dat, bool max, int* index=0)
 {
@@ -297,31 +229,33 @@ int main(int argc, char* argv[])
         }
 
 
-        double maxf=func_data[0].x;
-        func_params.f = maxf;
-        double maxI=If(min->x,&func_params);
-        double minf=maxf;
-        double minI=If(min->x,&func_params);
-
-        for(double f=func_data[0].x; f<func_data.last().x; f++)
         {
-            func_params.f = f;
-            double I=If(min->x,&func_params);
-            X_f.push_back(f);
-            Y_f.push_back(I);
-            if(I>maxI)
-            {
-                maxI=I;
-                maxf=f;
-            }
+            double maxf=func_data[0].x;
+            func_params.f = maxf;
+            double maxI=If(min->x,&func_params);
+            double minf=maxf;
+            double minI=If(min->x,&func_params);
 
-            if(I<minI)
+            for(double freq=func_data[0].x; freq<func_data.last().x; ++freq)
             {
-                minI=I;
-                minf=f;
+                func_params.f = freq;
+                double I=If(min->x,&func_params);
+                X_f.push_back(freq);
+                Y_f.push_back(I);
+                if(I>maxI)
+                {
+                    maxI=I;
+                    maxf=freq;
+                }
+
+                if(I<minI)
+                {
+                    minI=I;
+                    minf=freq;
+                }
             }
+            std::cout<<f.fileName().toAscii().data()<<"\t"<<maxf<<"\t"<<maxI<<"\t"<<minf<<"\t"<<minI<<"\t"<<min->fval<<"\t"<<noise<<"\n";
         }
-        std::cout<<f.fileName().toAscii().data()<<"\t"<<maxf<<"\t"<<maxI<<"\t"<<minf<<"\t"<<minI<<"\t"<<min->fval<<"\t"<<noise<<"\n";
 
         gsl_vector_memcpy(a, min->x);
 
