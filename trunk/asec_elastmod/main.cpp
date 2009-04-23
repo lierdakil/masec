13bf22e31738509fc3226231b5377b88e3b7c7ce
g.livid <g.livid@7c22be1c-a831-11dd-b08a-b13dd1f1ca2d>
2009-04-23 12:04:14 +0000

    asec_elastmod now works exclusively with stdin and stdout, with no other files.

--- trunk/asec_elastmod/main.cpp
+++ trunk/asec_elastmod/main.cpp
@@ -1,29 +1,31 @@
 #include <QtCore>
 #include <QtGui>
+#include <iostream>
 
-#define getparam(p) { QRegExp r=match(#p); if(r.indexIn(line)>=0) p=r.capturedTexts().at(1).toDouble(); }
-#define checkparam(p) if(p<0) p=QInputDialog::getDouble(0,"asec_elastmod",QString("Please enter value for %1").arg(#p))
+#define getparam(p) \
+{ \
+  QRegExp r(QString("%1\\s*=\\s*([0-9eE.]+)").arg(#p));\
+  if(r.indexIn(line)>=0) p=r.capturedTexts().at(1).toDouble(); }
+#define checkparam(p,u) \
+if(p<0) p=QInputDialog::getDouble(0, \
+                                  "asec_elastmod", \
+                                  QString("Please enter value for %1 in %2")\
+                                  .arg(#p).arg(#u),\
+                                  0,-2147483647,2147483647,10); \
+        std::cout<<"# "#p" = "<<p<<" "#u"\n"
 #define data_append(var) buf.setNum(var); data<<buf
-
-QRegExp match(QString name)
-{
-    return QRegExp(QString("%1\\s*=\\s*([0-9eE.]+)").arg(name));
-}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    if(argc!=3)
-        return 1;
-    QFile f(argv[1]);
-    QFile f2(argv[2]);
-    if(f.open(QIODevice::ReadOnly) && f2.open(QIODevice::WriteOnly))
+    QFile f;
+    if(f.open(0,QIODevice::ReadOnly))
     {
         double Mq=-1, Ms=-1, Fr=-1, ls=-1, hs=-1, ws=-1;
         QString line;
         do
         {
-            line=QString::fromLocal8Bit(f.readLine());
+            line=QString::fromLocal8Bit(f.readLine()).trimmed();
             if(line.startsWith('#'))
             {
                 getparam(Mq);
@@ -32,28 +34,35 @@ int main(int argc, char *argv[])
                 getparam(ls);
                 getparam(hs);
                 getparam(ws);
-                f2.write(line.toLocal8Bit());
+                //std::cout<<line.toLocal8Bit().data();
             }
         } while (line.startsWith('#'));
 
-        checkparam(Mq);
-        checkparam(Ms);
-        checkparam(Fr);
-        checkparam(ls);
-        checkparam(hs);
-        checkparam(ws);
+        checkparam(Mq,gm);
+        checkparam(Ms,gm);
+        checkparam(Fr,Hz);
+        checkparam(ls,cm);
+        checkparam(hs,cm);
+        checkparam(ws,cm);
 
-        line.remove("\n");
         QStringList header=line.split('\t');
         header<<"fs, Hz";
         header<<"E, dn,cm2";
         header<<"1/Q";
-        f2.write(header.join("\t").append("\n").toLocal8Bit());
-        while(!f.atEnd())
+        std::cout<<header.join("\t").append("\n").toLocal8Bit().data();
+        QByteArray buf;
+        while(true)
         {
-            line=QString::fromLocal8Bit(f.readLine());
-            line.remove("\n");
+            buf=f.readLine();
+            if(buf.isEmpty())
+                break;
+            line=QString::fromLocal8Bit(buf).trimmed();
+
+            if(line.isEmpty())
+                continue;
             QStringList data=line.split('\t');
+            if(data.count()<4)
+                continue;
             double A=1+Mq/Ms;
             double B=Mq*Fr/Ms;
             double fr=data.at(header.indexOf("Resonance freq, Hz")).toDouble();
@@ -67,8 +76,9 @@ int main(int argc, char *argv[])
             data_append(fs);
             data_append(E);
             data_append(Q1);
-            f2.write(data.join("\t").append("\n").toLocal8Bit());
+            std::cout<<data.join("\t").append("\n").toLocal8Bit().data();
         }
+        f.close();
         return 0;
     } else {
         return 2;
