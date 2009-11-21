9a49bceb0cd6736c595f9a77172a30b22f894aee
Nikolay Yakimov <root@livid.pp.ru>
2009-11-21 11:59:27 +0000

    Using QwtPlot now Added the ability to save setpoint(time) and temperature(time) to an arbitrary filename

--- asec_temperature/adaptors.h
+++ asec_temperature/adaptors.h
@@ -14,46 +14,49 @@
 class export_adaptor : public QDBusAbstractAdaptor
 {
     Q_OBJECT
-    Q_CLASSINFO("D-Bus Interface", QString("ru.pp.livid.asec.exports"))
+    Q_CLASSINFO("D-Bus Interface", QString("ru.pp.livid.asec.exports"));
 
 private:
-	vib_temperature *t;
+    vib_temperature *t;
 
 public:
     export_adaptor(vib_temperature *parent);
 
 public slots:
     void set_temp(double temp, double ramp, double timeout);
+    void set_filename(QString file);
 };
 
 class flow_adaptor : public QDBusAbstractAdaptor
 {
-	Q_OBJECT
-	Q_CLASSINFO("D-Bus Interface", QString("ru.pp.livid.asec.flow"))
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", QString("ru.pp.livid.asec.flow"));
+
 private:
-	vib_temperature *t;
-	double setpoint;
+    vib_temperature *t;
+    double setpoint;
 public:
-	flow_adaptor(vib_temperature *parent);
+    flow_adaptor(vib_temperature *parent);
 public slots:
     void stop();
 signals:
-	void finished(QStringList data);
+    void finished(QStringList data);
 };
 
 class help_adaptor : public QDBusAbstractAdaptor
 {
-    Q_OBJECT
-    Q_CLASSINFO("D-Bus Interface", QString("ru.pp.livid.asec.help"))
+    Q_OBJECT;
+    Q_CLASSINFO("D-Bus Interface", QString("ru.pp.livid.asec.help"));
 
 private:
-	vib_temperature *t;
+    vib_temperature *t;
 public:
     help_adaptor(vib_temperature *parent);
 public slots:
-        QString module_description();
-	QStringList returned_values();
+    QString module_description();
+    QStringList returned_values();
     QString set_temp();
+    QString set_filename();
 };
 
 #endif /* ADAPTORS_H_ */
--- asec_temperature/asec_temperature.pro
+++ asec_temperature/asec_temperature.pro
@@ -23,3 +23,14 @@ INCLUDEPATH += ../asec_gpib \
         ../visa
 LIBS += -L../build \
     -lasec_gpib
+
+win32 {
+    INCLUDEPATH += C:\Qwt-5.2.0-svn\include
+    LIBS += -L C:\Qwt-5.2.0-svn\lib
+}
+unix {
+    INCLUDEPATH += /usr/include \
+        /usr/include/qwt5 \
+        /usr/include/qwt-qt4
+}
+LIBS += -lqwt
