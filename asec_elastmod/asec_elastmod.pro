06ec1d394c0ea40c7924a7e86edf93a4313714b2
Nikolay Yakimov <root@livid.pp.ru>
2009-11-22 09:18:10 +0000

    A little code cleanup

--- asec_elastmod/asec_elastmod.pro
+++ asec_elastmod/asec_elastmod.pro
@@ -1,16 +1,11 @@
-#-------------------------------------------------
-#
+# -------------------------------------------------
 # Project created by QtCreator 2009-04-23T00:22:45
-#
-#-------------------------------------------------
-
+# -------------------------------------------------
 TARGET = asec_elastmod
-CONFIG   += console
-CONFIG   -= app_bundle
+CONFIG += console
+CONFIG -= app_bundle
 CONFIG += exceptions
 DESTDIR = ../build/
-
 TEMPLATE = app
-
-
 SOURCES += main.cpp
+HEADERS += defines.h
---
+++ asec_elastmod/defines.h
@@ -0,0 +1,19 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define getparam(p,u) \
+{ \
+  QRegExp r(QString("%1\\s*=\\s*([0-9eE.]+)\\s*([A-z]+)").arg(#p));\
+  if(r.indexIn(line)>=0) if(r.capturedTexts().at(2).toLower()==#u) p=r.capturedTexts().at(1).toDouble(); }
+
+#define checkparam(p,u) \
+if(p<0) p=QInputDialog::getDouble(0, \
+                                  "asec_elastmod", \
+                                  QString("Please enter value for %1 in %2")\
+                                  .arg(#p).arg(#u),\
+                                  0,-2147483647,2147483647,10); \
+          std::cout<<"# "#p" = "<<p<<" "#u"\n"
+
+#define data_append(var) data<<QString::number(var,'f')
+
+#endif // DEFINES_H
